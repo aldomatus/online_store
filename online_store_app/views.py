03daa3e224455957e5602596439c16ccf122590c
Aldo Matus <aldo.matus@outlook.com>
2021-05-16 16:50:49 -0500

    added main templates and project views in views.py

---
+++ online_store_app/views.py
@@ -0,0 +1,27 @@
+"""Views of my projet"""
+#   Django 
+from django.shortcuts import render, HttpResponse
+
+#   views
+def home(request):
+    
+    return render(request,"online_store_app/home.html")
+
+def services(request):
+    
+    return render(request,"online_store_app/services.html")
+
+def store(request):
+    
+    return render(request,"online_store_app/store.html")
+
+def blog(request):
+    
+    return render(request,"online_store_app/blog.html")
+
+def contact(request):
+    
+    return render(request,"online_store_app/contact.html")
+
+
+
